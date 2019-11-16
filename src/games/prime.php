3789f081576bf626a993b1c86ea21cb1acd21670
Vadim <shevyakin.vadim@gmail.com>
2019-11-16 09:48:05 +0000

    fixed  range

--- src/games/prime.php
+++ src/games/prime.php
@@ -4,7 +4,7 @@ namespace BrainGames\Prime;
 
 function generatePrimeData()
 {
-    $question = rand(1, 100);
+    $question = rand(2, 100);
     $primeNumbers = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97];
     $expectedAnswer = in_array($question, $primeNumbers) ? "yes" : "no";
     $result = [$question,$expectedAnswer];
