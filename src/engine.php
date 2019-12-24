692d57c9aee7b27a50412f29d627d24a3b51043a
Vadim <shevyakin.vadim@gmail.com>
2019-12-24 16:32:30 +0000

    refactoring attempt #1

--- src/engine.php
+++ src/engine.php
@@ -1,46 +1,28 @@
 <?php
 
 namespace BrainGames\Engine;
-
+/*
 use function BrainGames\Even\generateEvenData;
 use function BrainGames\Calc\generateCalcData;
 use function BrainGames\GCD\generateGCDData;
 use function BrainGames\Progression\generateProgressionData;
 use function BrainGames\Prime\generatePrimeData;
+*/
 use function cli\line;
 use function cli\prompt;
 
-function engine($game)
+function engine($question, $expectedAnswer)
 {
-    $correctAnswers = 0;
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
-    while ($correctAnswers < 3) {
-        if ($game === 'even') {
-            [$question,$expectedAnswer] = generateEvenData();
-        }
-        if ($game === 'calc') {
-            [$question,$expectedAnswer] = generateCalcData();
-        }
-        if ($game === 'gcd') {
-            [$question,$expectedAnswer] = generateGCDData();
-        }
-        if ($game === 'progression') {
-            [$question,$expectedAnswer] = generateProgressionData();
-        }
-        if ($game === 'prime') {
-            [$question,$expectedAnswer] = generatePrimeData();
-        }
-        line("Question:%s", $question);
-        $answer = prompt("Your answer");
-        if ($answer === $expectedAnswer) {
-            line("Correct!");
-            $correctAnswers += 1;
-        } else {
-            line("$answer is wrong answer, correct answer was $expectedAnswer");
-            line("Let's try again, $name");
-            $correctAnswers = 0;
-        }
+    line("Question:%s", $question);
+    $answer = prompt("Your answer");
+    if ($answer === $expectedAnswer) {
+        line("Correct!");
+    #    return "success";
+    } else {
+        line("$answer is wrong answer, correct answer was $expectedAnswer");
+        line("Let's try again, $name");
+    #    return "fail";
     }
-    line("Congratulations, $name!");
 }
--- src/games/even.php
+++ src/games/even.php
@@ -1,11 +1,21 @@
 <?php
 
 namespace BrainGames\Even;
+use function BrainGames\Engine\engine;
 
 function generateEvenData()
 {
+    echo "Welcome to the Brain Games!\n";
+    echo "Answer \"yes\" if the number is even, otherwise answer \"no\".\n";
+    function isEven($num)
+    {
+    return $num % 2 ? 'no' : 'yes';
+    }
+
+    $correctAnswersCount = 0;
+    while($correctAnswersCount < 3) {
     $question = rand(1, 100);
-    $expectedAnswer = $question % 2 ? 'no' : 'yes';
-    $result = [$question,$expectedAnswer];
-    return $result;
+    $expectedAnswer = isEven($question);
+    engine($question, $expectedAnswer);
+    }
 }
