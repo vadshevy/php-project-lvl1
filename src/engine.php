3cc2707c756d6c2064cfe6c0711fdd8a15f4ccc7
Vadim <shevyakin.vadim@gmail.com>
2020-01-20 17:23:02 +0000

    removed  counter from engine.php

--- src/engine.php
+++ src/engine.php
@@ -12,7 +12,7 @@ function engine($gameData, $intro)
     print_r($intro);
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
-    $correctAnswers = 0;
+#    $correctAnswers = 0;
     $rounds = 3;
     for ($i = 1; $i <= $rounds; $i++) {
         [$question, $expectedAnswer] = $gameData();
@@ -20,7 +20,7 @@ function engine($gameData, $intro)
         $answer = prompt("Your answer");
         if ($answer === $expectedAnswer) {
             line("Correct!");
-            $correctAnswers += 1;
+#            $correctAnswers += 1;
         } else {
             line("$answer is wrong answer, correct answer was $expectedAnswer");
             line("Let's try again, $name");
